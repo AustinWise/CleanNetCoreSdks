3a20080847e76de7b5ce0ec228b119eb3de4f393
Austin Wise <AustinWise@gmail.com>
2019-04-21 18:34:10 -0700

    Argument parsing.

--- CleanNetCoreSdks/Program.cs
+++ CleanNetCoreSdks/Program.cs
@@ -1,32 +1,75 @@
-﻿using System;
+﻿using Mono.Options;
+using System;
 using System.Linq;
 
 namespace Austin.CleanNetCoreSdks
 {
     class Program
     {
+        const int EXIT_SUCCESS = 0;
+        const int EXIT_CRASH = 1;
+        const int EXIT_FAIL = 2;
+        const int EXIT_ARGS = 3;
+
+        static void Usage(OptionSet opts)
+        {
+            opts.WriteOptionDescriptions(Console.Error);
+        }
+
         static int Main(string[] args)
         {
+            var prog = new Program();
+
+            var opts = new OptionSet()
+            {
+                { "ignore-visual-studio", "Do not pin SDK version bands pinned by Visual Studio.", (bool v) => prog.IgnoreVisualStudio = v },
+                { "pin-by-runtime", "Pin by included runtime version rather than SDK band.", (bool v) => prog.KeepOnlyLastVersionPerRuntime = v },
+            };
+
             try
             {
-                new Program().Run(args);
-                return 0;
+                var extra = opts.Parse(args);
+                if (extra.Count != 0)
+                {
+                    Console.Error.WriteLine("Unexpected options:");
+                    foreach (var a in extra)
+                    {
+                        Console.Error.WriteLine("\t" + extra);
+                    }
+                    Console.Error.WriteLine();
+                    Usage(opts);
+                    return EXIT_ARGS;
+                }
+
+                prog.Run();
+                return EXIT_SUCCESS;
+            }
+            catch (OptionException ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                Console.Error.WriteLine();
+                Usage(opts);
+                return EXIT_ARGS;
             }
             catch (ExitException ex)
             {
                 Console.Error.WriteLine("Program failed:");
                 Console.Error.WriteLine(ex.Message);
-                return 2;
+                return EXIT_FAIL;
             }
             catch (Exception ex)
             {
                 Console.Error.WriteLine("PROGRAM CRASHED:");
                 Console.Error.WriteLine(ex.ToString());
-                return 1;
+                return EXIT_CRASH;
             }
         }
 
-        void Run(string[] args)
+        bool IgnoreVisualStudio { get; set; }
+
+        bool KeepOnlyLastVersionPerRuntime { get; set; }
+
+        void Run()
         {
             var sdks = DotnetSdk.GetInstalledSdks().OrderBy(s => s.Version).ToArray();
             Console.WriteLine("Installed SDKs:");
