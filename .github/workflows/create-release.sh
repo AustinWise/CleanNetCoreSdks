f72be04fa8178fccfe55ec19a192423ac169c86c
Austin Wise <AustinWise@gmail.com>
2023-12-29 13:10:08 -0800

    Use the gh command to create the release at the end

---
+++ .github/workflows/create-release.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# This script takes following arguments:
+#   1. The assembly name
+#   2. A path to a directory containing the downloaded artifacts.
+# This script can be tested on your local computer by downloading the artifacts with the
+# `gh run download` command.
+
+set -euo pipefail
+set -x
+
+if [[ ! -d $2 ]]; then
+    echo "Missing directory argument"
+    exit 1
+fi
+
+assembly_name=$1
+temp_dir=$2
+
+for d in $temp_dir/program-*/; do
+    cp README.md $d/
+    cp LICENSE $d/
+    pushd $d
+    if [[ $d =~ "/program-win" ]]; then
+        zip -r "../$(basename $d | sed -e 's/^program-//').zip" *
+    else
+        echo skip
+        chmod +x "$assembly_name"
+        tar czf "../$(basename $d | sed -e 's/^program-//' -e 's/^osx-/macos-/').tar.gz" *
+    fi
+    popd
+done
